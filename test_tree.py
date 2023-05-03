4a1400dfcc3d6041c177e6855502f002ae52cfe1
student <tudor.diaconu@stud.acs.upb.ro>
2023-05-03 16:55:32 +0300

    fix

--- test_tree.py
+++ test_tree.py
@@ -5,7 +5,7 @@ copac = tree.Tree()
 
 def test_find():
         copac.add(8)
-        assert copac.find(8).data is 8
+        assert copac.find(8).data == 8
 
 def test_find_not():
     copac.add(8)
