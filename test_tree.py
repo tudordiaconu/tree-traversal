5b89eafef6ac52e3c8401bf53752da403edadb17
student <tudor.diaconu@stud.acs.upb.ro>
2023-05-03 17:20:42 +0300

    plm

--- test_tree.py
+++ test_tree.py
@@ -4,9 +4,9 @@ import tree
 copac = tree.Tree()
 
 def test_find():
-        copac.add(8)
-        assert copac.find(8).data == 8
+    copac.add(8)
+    assert copac.find(8).data == 8
 
 def test_find_not():
     copac.add(8)
-    assert copac.find(5) is None
+    assert copac.find(8).data == 7
